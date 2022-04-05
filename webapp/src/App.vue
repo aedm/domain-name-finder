8196094f9a85c4192576d4e11343e6fe363cd01e
aedm <korteur@gmail.com>
2022-04-05 14:15:32 +0200

    Specify minimum and maximum length

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,24 +1,51 @@
 <script setup lang='ts'>
 import {computed, ref, watchEffect} from 'vue';
 import Textbox from '@/components/Textbox.vue';
-import {SearchInput, useSearch} from '@/lib/searchHook';
-import {asType} from "@/lib/asType";
+import {useSearch} from '@/lib/searchHook';
+import ButtonGroup from "@/components/ButtonGroup.vue";
+
+const lengthOptions = new Map<number, string>();
+lengthOptions.set(1, 'one');
+lengthOptions.set(2, 'two');
+lengthOptions.set(3, 'three');
+const minWordCount = ref(1);
+const maxWordCount = ref(2);
 
 const words = ref('enter some words here ');
 const prefixes = ref('prefix ');
 const postfixes = ref('postfix ');
+
 const {setInput, result} = useSearch();
 const free = computed(() => result.value?.free);
 const reserved = computed(() => result.value?.reserved);
 
 watchEffect(() => {
-  setInput({postfixes: postfixes.value, prefixes: prefixes.value, words: words.value});
+  setInput({
+    maxWordCount: maxWordCount.value,
+    minWordCount: minWordCount.value,
+    postfixes: postfixes.value,
+    prefixes: prefixes.value,
+    words: words.value
+  });
+});
+
+watchEffect(() => {
+  console.log("length", minWordCount.value, maxWordCount.value);
+  console.log("ee", typeof minWordCount.value );
 });
 
 </script>
 
 <template>
   <div class='container mx-auto mt-6'>
+    <div class="my-2">
+      At least
+      <ButtonGroup v-model="minWordCount" :options="lengthOptions"/>
+      and at most
+      <ButtonGroup v-model="maxWordCount" :options="lengthOptions"/>
+      words.
+    </div>
+
     <Textbox v-model='prefixes'/>
     <Textbox v-model='words'/>
     <Textbox v-model='postfixes'/>

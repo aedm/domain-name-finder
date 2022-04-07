c8d07fb587b07457f793b535e87ead7e646540ff
aedm <korteur@gmail.com>
2022-04-07 17:36:59 +0200

    Download file

--- tools/src/bin/download_zone_file.rs
+++ tools/src/bin/download_zone_file.rs
@@ -1,8 +1,13 @@
 use anyhow::{Context, Result};
 use chrono::DateTime;
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::{Method, Response};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::cmp::min;
+use std::fs::File;
+use std::io::Write;
 use tools::{fetch_json, get_env, send_request};
 
 const AUTH_URL: &str = &"https://account-api.icann.org/api/authenticate";
@@ -48,6 +53,44 @@ async fn fetch_headers(access_token: &str) -> Result<()> {
     //     .header("Authorization", format!("bearer {access_token}"));
 }
 
+async fn download_zone_file(access_token: &str) -> Result<()> {
+    // Reqwest setup
+    let res = send_request(ZONE_FILE_URL, access_token, Method::HEAD, &json!({})).await?;
+
+    // let res = client
+    //     .get(url)
+    //     .send()
+    //     .await
+    //     .or(Err(format!("Failed to GET from '{}'", &url)))?;
+    let total_size = res
+        .content_length()
+        .context("Failed to get content length")?;
+
+    // Indicatif setup
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading {}", ZONE_FILE_URL));
+
+    // download chunks
+    let path = "com.zone.txt.gz";
+    let mut file = File::create(path)?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        file.write_all(&chunk)?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(format!("Downloaded {} to {}", ZONE_FILE_URL, path));
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // let aws = get_env("AWS_C")?;
@@ -57,7 +100,7 @@ async fn main() -> Result<()> {
     // println!("Access token: {token}");
 
     let token = "eyJraWQiOiJFczQ4dzhadTZISjhxd2F1M3M1bjJhMUUtSFN1Tk5PbW00Tl9oU0JwYW1RIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmw2OU1ZZlhsSW5HbDVFNzJibTIxZmx3cThsRXlsbkRuQ2Zqck1YaXFSc28iLCJpc3MiOiJodHRwczovL2ljYW5uLWFjY291bnQub2t0YS5jb20vb2F1dGgyL2F1czJwMDFjMnJvSkFlQ2dZMnA3IiwiYXVkIjoiaHR0cDovL2FwaV9hdXRoZW5yaXphdGlvbl9zZXJ2ZXIuaWNhbm4ub3JnIiwiaWF0IjoxNjQ5MzM4MjMyLCJleHAiOjE2NDk0MjQ2MzIsImNpZCI6IjBvYTFyY2prcWtPbGlNUHVMMnA3IiwidWlkIjoiMDB1ZWhrNGw4a21oRzZVN2EycDciLCJzY3AiOlsiaWNhbm4tY3VzdG9tIiwib3BlbmlkIl0sImF1dGhfdGltZSI6MTY0OTMzODIzMiwic3ViIjoia29ydGV1ckBnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiR8OhYm9yIiwiZmFtaWx5X25hbWUiOiJHeWVibsOhciIsImVtYWlsIjoia29ydGV1ckBnbWFpbC5jb20ifQ.fVxH1PbOw1ls880eJJCpLOIxVdOTsz1u4xUgUUFLc3rK6TEWO-RCFNm2rissRqIoFmcVvQ9fU1D7mTELGlYsc6YxqsqoJlvtUQR1SsavelnmlzwE0oaxYVvVn9iHmjbeVCHd6MuEksoA_-W886hEvJ8kAounbG81KehObM1cq82jwTUzEGn2uJN-bTPLeLZAcA9J6O71YqhJwrgOzS_O9bBprmnctXq65zlfwtBY468TvtF7Efpt3_XmP1D9lmNcKTTso3rhfd1xnc92F3aIb35Zc2vBuGBQppATHCKp00NqAIoC_pfNv6v25McikEpp6gPY3gc1w-uZK2KybeIBfg";
-    fetch_headers(token).await?;
-
+    // fetch_headers(token).await?;
+    download_zone_file(token).await?;
     Ok(())
 }

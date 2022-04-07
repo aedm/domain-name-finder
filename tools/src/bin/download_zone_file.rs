13541dcc209c3fa498553a283fe1f4608f48a3be
aedm <korteur@gmail.com>
2022-04-07 22:08:51 +0200

    Hack

--- tools/src/bin/download_zone_file.rs
+++ tools/src/bin/download_zone_file.rs
@@ -1,6 +1,5 @@
 use anyhow::{Context, Result};
 use chrono::DateTime;
-use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::{Method, Response};
 use serde::{Deserialize, Serialize};
@@ -8,7 +7,7 @@ use serde_json::json;
 use std::cmp::min;
 use std::fs::File;
 use std::io::Write;
-use tools::{fetch_json, get_env, send_request};
+use tools::{download_stream_to_file, fetch_json, get_env, send_request};
 
 const AUTH_URL: &str = &"https://account-api.icann.org/api/authenticate";
 const ZONE_FILE_URL: &str = &"https://czds-api.icann.org/czds/downloads/com.zone";
@@ -26,12 +25,14 @@ async fn fetch_access_token(username: &str, password: &str) -> Result<String> {
         password: &'a str,
     }
 
-    let response: AuthResponse = fetch_json(AUTH_URL, &AuthRequest { username, password }).await?;
+    let response: AuthResponse =
+        fetch_json(AUTH_URL, None, &AuthRequest { username, password }).await?;
     Ok(response.access_token)
 }
 
 async fn fetch_headers(access_token: &str) -> Result<()> {
-    let response = send_request(ZONE_FILE_URL, access_token, Method::HEAD, &json!({})).await?;
+    let response =
+        send_request(ZONE_FILE_URL, Some(access_token), Method::HEAD, &json!({})).await?;
     println!("{response:#?}");
 
     let last_modified = response
@@ -54,41 +55,10 @@ async fn fetch_headers(access_token: &str) -> Result<()> {
 }
 
 async fn download_zone_file(access_token: &str) -> Result<()> {
-    // Reqwest setup
-    let res = send_request(ZONE_FILE_URL, access_token, Method::GET, &json!({})).await?;
-
-    // let res = client
-    //     .get(url)
-    //     .send()
-    //     .await
-    //     .or(Err(format!("Failed to GET from '{}'", &url)))?;
-    let total_size = res
-        .content_length()
-        .context("Failed to get content length")?;
-
-    // Indicatif setup
-    let pb = ProgressBar::new(total_size);
-    pb.set_style(ProgressStyle::default_bar()
-        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
-        .progress_chars("#>-"));
-    pb.set_message(format!("Downloading {}", ZONE_FILE_URL));
-
-    // download chunks
-    let path = "com.zone.txt.gz";
-    let mut file = File::create(path)?;
-    let mut downloaded: u64 = 0;
+    let res = send_request(ZONE_FILE_URL, Some(access_token), Method::GET, &json!({})).await?;
     let mut stream = res.bytes_stream();
-
-    while let Some(item) = stream.next().await {
-        let chunk = item?;
-        file.write_all(&chunk)?;
-        let new = min(downloaded + (chunk.len() as u64), total_size);
-        downloaded = new;
-        pb.set_position(new);
-    }
-
-    pb.finish_with_message(format!("Downloaded {} to {}", ZONE_FILE_URL, path));
-    Ok(())
+    stream.next();
+    download_stream_to_file(stream).await
 }
 
 #[tokio::main]
@@ -96,11 +66,11 @@ async fn main() -> Result<()> {
     // let aws = get_env("AWS_C")?;
     let icann_username = get_env("ICANN_USERNAME")?;
     let icann_password = get_env("ICANN_PASSWORD")?;
-    // let token = fetch_access_token(&icann_username, &icann_password).await?;
-    // println!("Access token: {token}");
+    let token = fetch_access_token(&icann_username, &icann_password).await?;
+    println!("Access token: {token}");
 
-    let token = "eyJraWQiOiJFczQ4dzhadTZISjhxd2F1M3M1bjJhMUUtSFN1Tk5PbW00Tl9oU0JwYW1RIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmw2OU1ZZlhsSW5HbDVFNzJibTIxZmx3cThsRXlsbkRuQ2Zqck1YaXFSc28iLCJpc3MiOiJodHRwczovL2ljYW5uLWFjY291bnQub2t0YS5jb20vb2F1dGgyL2F1czJwMDFjMnJvSkFlQ2dZMnA3IiwiYXVkIjoiaHR0cDovL2FwaV9hdXRoZW5yaXphdGlvbl9zZXJ2ZXIuaWNhbm4ub3JnIiwiaWF0IjoxNjQ5MzM4MjMyLCJleHAiOjE2NDk0MjQ2MzIsImNpZCI6IjBvYTFyY2prcWtPbGlNUHVMMnA3IiwidWlkIjoiMDB1ZWhrNGw4a21oRzZVN2EycDciLCJzY3AiOlsiaWNhbm4tY3VzdG9tIiwib3BlbmlkIl0sImF1dGhfdGltZSI6MTY0OTMzODIzMiwic3ViIjoia29ydGV1ckBnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiR8OhYm9yIiwiZmFtaWx5X25hbWUiOiJHeWVibsOhciIsImVtYWlsIjoia29ydGV1ckBnbWFpbC5jb20ifQ.fVxH1PbOw1ls880eJJCpLOIxVdOTsz1u4xUgUUFLc3rK6TEWO-RCFNm2rissRqIoFmcVvQ9fU1D7mTELGlYsc6YxqsqoJlvtUQR1SsavelnmlzwE0oaxYVvVn9iHmjbeVCHd6MuEksoA_-W886hEvJ8kAounbG81KehObM1cq82jwTUzEGn2uJN-bTPLeLZAcA9J6O71YqhJwrgOzS_O9bBprmnctXq65zlfwtBY468TvtF7Efpt3_XmP1D9lmNcKTTso3rhfd1xnc92F3aIb35Zc2vBuGBQppATHCKp00NqAIoC_pfNv6v25McikEpp6gPY3gc1w-uZK2KybeIBfg";
+    // let token = "eyJraWQiOiJFczQ4dzhadTZISjhxd2F1M3M1bjJhMUUtSFN1Tk5PbW00Tl9oU0JwYW1RIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmw2OU1ZZlhsSW5HbDVFNzJibTIxZmx3cThsRXlsbkRuQ2Zqck1YaXFSc28iLCJpc3MiOiJodHRwczovL2ljYW5uLWFjY291bnQub2t0YS5jb20vb2F1dGgyL2F1czJwMDFjMnJvSkFlQ2dZMnA3IiwiYXVkIjoiaHR0cDovL2FwaV9hdXRoZW5yaXphdGlvbl9zZXJ2ZXIuaWNhbm4ub3JnIiwiaWF0IjoxNjQ5MzM4MjMyLCJleHAiOjE2NDk0MjQ2MzIsImNpZCI6IjBvYTFyY2prcWtPbGlNUHVMMnA3IiwidWlkIjoiMDB1ZWhrNGw4a21oRzZVN2EycDciLCJzY3AiOlsiaWNhbm4tY3VzdG9tIiwib3BlbmlkIl0sImF1dGhfdGltZSI6MTY0OTMzODIzMiwic3ViIjoia29ydGV1ckBnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiR8OhYm9yIiwiZmFtaWx5X25hbWUiOiJHeWVibsOhciIsImVtYWlsIjoia29ydGV1ckBnbWFpbC5jb20ifQ.fVxH1PbOw1ls880eJJCpLOIxVdOTsz1u4xUgUUFLc3rK6TEWO-RCFNm2rissRqIoFmcVvQ9fU1D7mTELGlYsc6YxqsqoJlvtUQR1SsavelnmlzwE0oaxYVvVn9iHmjbeVCHd6MuEksoA_-W886hEvJ8kAounbG81KehObM1cq82jwTUzEGn2uJN-bTPLeLZAcA9J6O71YqhJwrgOzS_O9bBprmnctXq65zlfwtBY468TvtF7Efpt3_XmP1D9lmNcKTTso3rhfd1xnc92F3aIb35Zc2vBuGBQppATHCKp00NqAIoC_pfNv6v25McikEpp6gPY3gc1w-uZK2KybeIBfg";
     // fetch_headers(token).await?;
-    download_zone_file(token).await?;
+    download_zone_file(&token).await?;
     Ok(())
 }
--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -1,4 +1,11 @@
 use anyhow::{anyhow, Result};
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Method;
+use std::cmp::min;
+use std::fs::File;
+use std::io::Write;
+use std::str::Bytes;
 
 pub fn get_env(name: &str) -> Result<String> {
     if let Ok(var) = std::env::var(name) {
@@ -9,31 +16,57 @@ pub fn get_env(name: &str) -> Result<String> {
     Err(anyhow!("Missing environment variable '{name}'."))
 }
 
-pub async fn fetch_json<Req: serde::ser::Serialize, Resp: serde::de::DeserializeOwned>(
-    url: &str,
-    request_payload: &Req,
-) -> Result<Resp> {
-    let client = reqwest::Client::new();
-    let res = client
-        .post(url)
-        .json(&request_payload)
-        .header("User-Agent", "utils/0.1.0")
-        .send()
-        .await?;
-    Ok(res.json::<Resp>().await?)
-}
-
 pub async fn send_request<Req: serde::ser::Serialize>(
     url: &str,
-    access_token: &str,
+    access_token: Option<&str>,
     method: reqwest::Method,
     payload: &Req,
 ) -> Result<reqwest::Response, reqwest::Error> {
-    reqwest::Client::new()
+    let mut request = reqwest::Client::new()
         .request(method, url)
-        .header("User-Agent", "utils/0.1.0")
-        .header("Authorization", format!("bearer {access_token}"))
-        .json(payload)
-        .send()
-        .await
+        .header("User-Agent", "utils/0.1.0");
+    if let Some(token) = access_token {
+        request = request.header("Authorization", format!("bearer {token}"));
+    }
+    request.json(payload).send().await
+}
+
+pub async fn fetch_json<Req: serde::ser::Serialize, Resp: serde::de::DeserializeOwned>(
+    url: &str,
+    access_token: Option<&str>,
+    request_payload: &Req,
+) -> Result<Resp> {
+    let response = send_request(url, access_token, Method::POST, request_payload).await?;
+    Ok(response.json::<Resp>().await?)
+}
+
+pub async fn download_stream_to_file(
+    mut stream: impl futures_core::Stream<Item = reqwest::Result<Bytes>>,
+) -> Result<()> {
+    let total_size = res
+        .content_length()
+        .context("Failed to get content length")?;
+
+    // Indicatif setup
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading {}", ZONE_FILE_URL));
+
+    // download chunks
+    let path = "com.zone.txt.gz";
+    let mut file = File::create(path)?;
+    let mut downloaded: u64 = 0;
+
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        file.write_all(&chunk)?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(format!("Downloaded {} to {}", ZONE_FILE_URL, path));
+    Ok(())
 }

56ea4adb080f05fd5476a14cd6918e68c277041e
aedm <korteur@gmail.com>
2022-04-09 14:41:30 +0200

    tokio::io::copy & progress bar

--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -1,14 +1,15 @@
 use anyhow::{anyhow, Context, Result};
-// use tokio_util::io::StreamReader;
-use futures_util::StreamExt;
 use futures_util::TryStreamExt;
+use futures_util::{pin_mut, StreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Method;
+use std::cell::RefCell;
 use std::cmp::min;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Error, ErrorKind, Write};
+use std::rc::Rc;
 use std::str::Bytes;
-// use tokio_util::io::StreamReader;
+use tokio_util::io::StreamReader;
 
 pub fn get_env(name: &str) -> Result<String> {
     if let Ok(var) = std::env::var(name) {
@@ -43,6 +44,10 @@ pub async fn fetch_json<Req: serde::ser::Serialize, Resp: serde::de::Deserialize
     Ok(response.json::<Resp>().await?)
 }
 
+fn type_of<T>(_: T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
+
 pub async fn download_stream_to_file(mut res: reqwest::Response, path: &str) -> Result<()> {
     let total_size = res
         .content_length()
@@ -54,28 +59,42 @@ pub async fn download_stream_to_file(mut res: reqwest::Response, path: &str) -> 
         .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         .progress_chars("#>-"));
     pb.set_message(format!("Downloading {}", path));
+    let pb = Rc::new(RefCell::new(pb));
+    let pb_clone = pb.clone();
 
     // download chunks
     let mut stream = res.bytes_stream();
-    let mut file = File::create(path)?;
-    let mut downloaded: u64 = 0;
+    // let mut rrr = StreamReader::new(stream.map(|data| -> Result<_, std::io::Error> {
+    //     // let chunk = item?;
+    //     if let Ok(chunk) = data {
+    //         pb_clone.borrow_mut().inc(chunk.len() as u64);
+    //         return Ok(data);
+    //     }
+    //     Err(std::io::Error::new(ErrorKind::Other, "oh no!"))
+    // }));
 
     // TODO: download speed is slow for some reason
-    while let Some(item) = stream.next().await {
-        let chunk = item?;
-        file.write_all(&chunk)?;
-        let new = min(downloaded + (chunk.len() as u64), total_size);
-        downloaded = new;
-        pb.set_position(new);
+    let pb_stream = async_stream::stream! {
+        while let Some(item) = stream.next().await {
+            let chunk = item?;
+            // file.write_all(&chunk)?;
+            // let new = min(downloaded + (chunk.len() as u64), total_size);
+            // downloaded += chunk.len() as u64;
+            // pb_clone.borrow_mut().set_position(downloaded);
+            pb_clone.borrow_mut().inc(chunk.len() as u64);
+            yield Ok(chunk);
+        }
+    };
+    pin_mut!(pb_stream);
+
+    let mut file = tokio::fs::File::create(path).await?;
+    fn convert_err(err: reqwest::Error) -> std::io::Error {
+        todo!()
     }
+    let mut reader = StreamReader::new(pb_stream.map_err(convert_err));
+    tokio::io::copy(&mut reader, &mut file).await?;
 
-    // let mut file = tokio::fs::File::create(path).await?;
-    // fn convert_err(err: reqwest::Error) -> std::io::Error {
-    //     todo!()
-    // }
-    // let mut reader = StreamReader::new(stream.map_err(convert_err));
-    // tokio::io::copy(&mut reader, &mut file).await?;
-
-    pb.finish_with_message(format!("Downloaded {}", path));
+    pb.borrow_mut()
+        .finish_with_message(format!("Downloaded {}", path));
     Ok(())
 }

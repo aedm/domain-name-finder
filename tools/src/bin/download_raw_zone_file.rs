4b66281427e5f465e6719b207b1738f3645307af
aedm <korteur@gmail.com>
2022-06-01 23:09:29 +0200

    Downloads raw file

---
+++ tools/src/bin/download_raw_zone_file.rs
@@ -0,0 +1,134 @@
+extern crate dotenv;
+
+use anyhow::{anyhow, Context, Result};
+use chrono::DateTime;
+use dotenv::dotenv;
+use futures::stream::TryStreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use itertools::Itertools;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::io::ErrorKind;
+use tools::process_zone_file::process_zone_file;
+use tools::{
+    fetch_json, get_all_files_from_s3_bucket, get_env, make_aws_s3_client, response_to_reader,
+    send_request, ObservedReader,
+};
+
+const AUTH_URL: &str = &"https://account-api.icann.org/api/authenticate";
+const ZONE_FILE_URL: &str = &"https://czds-api.icann.org/czds/downloads/com.zone";
+const DATE_FORMAT: &str = "%Y%m%d-%H%M%S";
+const S3_FILE_EX: &str = ".txt.gz";
+
+async fn fetch_access_token(username: &str, password: &str) -> Result<String> {
+    #[derive(Deserialize, Debug)]
+    struct AuthResponse {
+        #[serde(rename = "accessToken")]
+        access_token: String,
+    }
+
+    #[derive(Serialize, Debug)]
+    struct AuthRequest<'a> {
+        username: &'a str,
+        password: &'a str,
+    }
+
+    let response: AuthResponse =
+        fetch_json(AUTH_URL, None, &AuthRequest { username, password }).await?;
+    Ok(response.access_token)
+}
+
+// async fn fetch_latest_s3_zone_date(
+//     client: &aws_sdk_s3::Client,
+//     bucket_name: &str,
+// ) -> Result<Option<String>> {
+//     let files = get_all_files_from_s3_bucket(client, bucket_name).await?;
+//
+//     // TODO: use a regex match instead of ends_with
+//     let last_file = files
+//         .into_iter()
+//         .filter(|s| s.ends_with(S3_FILE_EX))
+//         .sorted()
+//         .last();
+//     if let Some(s) = last_file {
+//         let date_str = &s[0..(s.len() - S3_FILE_EX.len())];
+//         return Ok(Some(date_str.into()));
+//     }
+//     Ok(None)
+// }
+
+fn get_file_date_from_header(response: &reqwest::Response) -> Result<String> {
+    let headers = response.headers();
+    let last_modified_orig = headers
+        .get("last-modified")
+        .context("Missing header: 'last-modified'")?
+        .to_str()?;
+    let last_icann_date = DateTime::parse_from_rfc2822(last_modified_orig)?
+        .format(DATE_FORMAT)
+        .to_string();
+    println!("Zone file date {last_icann_date:?}");
+    Ok(last_icann_date)
+}
+
+async fn download_zone_file(access_token: &str) -> Result<()> {
+    let mut response =
+        send_request(ZONE_FILE_URL, Some(access_token), Method::GET, &json!({})).await?;
+    let last_icann_date = get_file_date_from_header(&response)?;
+    //
+    // if let Some(last_processed_date) = latest {
+    //     if last_processed_date >= last_icann_date.as_str() {
+    //         return Err(anyhow!("ICANN zone file ({last_icann_date}) older than last processed ({last_processed_date})."));
+    //     }
+    // }
+    //
+    // if let Err(err) = std::fs::create_dir("db") {
+    //     if err.kind() != ErrorKind::AlreadyExists {
+    //         return Err(anyhow!("Can't create db directory: {err:?}"));
+    //     }
+    // }
+    //
+    let path = format!("./db/com.zone.{last_icann_date}.txt.gz");
+    // process_zone_file(response, &path).await?;
+    let total_size = response.content_length().context(format!(
+        "Failed to get content length from '{}'",
+        ZONE_FILE_URL
+    ))?;
+
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading {}", path));
+
+    let mut target_file = tokio::fs::File::create(format!("{path}")).await?;
+    let mut reader = response_to_reader(response);
+    let mut buf_reader = tokio::io::BufReader::new(reader);
+    let mut sum = 0;
+    let mut observer = ObservedReader::new(buf_reader, |buf| {
+        // println!("Buf size: {}", buf.filled().len());
+        let last = sum / 10_000_000;
+        sum += buf.filled().len() as u64;
+        if sum / 10_000_000 != last {
+            pb.set_position(sum);
+        }
+    });
+    tokio::io::copy(&mut observer, &mut target_file).await?;
+    pb.finish_with_message(format!("Downloaded {}", path));
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Starting zone file downloader.");
+    let _ = dotenv();
+
+    let icann_username = get_env("ICANN_USERNAME")?;
+    let icann_password = get_env("ICANN_PASSWORD")?;
+    let token = fetch_access_token(&icann_username, &icann_password).await?;
+    println!("TOKEN: {token}");
+    download_zone_file(&token).await?;
+    println!("DOne.");
+    Ok(())
+}
--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -2,8 +2,13 @@ pub mod process_zone_file;
 
 use anyhow::{anyhow, Result};
 use aws_config::meta::region::RegionProviderChain;
+use futures::stream::TryStreamExt;
 use itertools::Itertools;
+use pin_project::pin_project;
 use reqwest::Method;
+use std::pin::Pin;
+use std::task::Poll;
+use tokio::io::{AsyncRead, ReadBuf};
 
 pub fn get_env(name: &str) -> Result<String> {
     if let Ok(var) = std::env::var(name) {
@@ -51,4 +56,49 @@ pub async fn get_all_files_from_s3_bucket(
     let objects = client.list_objects_v2().bucket(bucket_name).send().await?;
     let contents = objects.contents.unwrap_or_default();
     Ok(contents.into_iter().map(|i| i.key).flatten().collect_vec())
+}
+
+pub fn response_to_reader(response: reqwest::Response) -> impl tokio::io::AsyncRead {
+    let stream = response
+        .bytes_stream()
+        .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e));
+    let mut stream_reader = tokio_util::io::StreamReader::new(stream);
+    stream_reader
+}
+
+/// An async reader which allows a closure to observe the bytes being read as
+/// they are ready. This has use cases such as hashing the output of a reader
+/// without interfering with the actual content.
+#[pin_project]
+pub struct ObservedReader<R, F> {
+    #[pin]
+    inner: R,
+    f: F,
+}
+
+impl<R, F> ObservedReader<R, F>
+where
+    R: AsyncRead,
+    F: FnMut(&ReadBuf),
+{
+    pub fn new(inner: R, f: F) -> Self {
+        Self { inner, f }
+    }
+}
+
+impl<R, F> AsyncRead for ObservedReader<R, F>
+where
+    R: AsyncRead,
+    F: FnMut(&ReadBuf),
+{
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        let this = self.as_mut().project();
+        futures::ready!(this.inner.poll_read(cx, buf))?;
+        (this.f)(buf);
+        Poll::Ready(Ok(()))
+    }
 }
